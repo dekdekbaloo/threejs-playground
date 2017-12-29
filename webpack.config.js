0020b7ec70c1e1f142fdc80e180c9d6c3df01ea4
Jessada Trirongkit <jessada.trirongkit@gmail.com>
2017-12-29 13:05:42 +0700

    Provide meaningful log messages

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const chalk = require('chalk')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin')
 
@@ -11,13 +12,31 @@ const entries = {
     js: './src/steer/index.js'
   }
 }
-
-module.exports = {
-  entry: Object.entries(entries)
+function generateEntryPoints (entries) {
+  console.log('. Generating entry points.')
+  return Object.entries(entries)
     .reduce((entry, [ name, { js } ]) => {
+      console.log(chalk.cyan('entry:'), name, chalk.cyan('js:'), js)
       entry[name] = js
       return entry
-    }, { }),
+    }, { })
+}
+function generateHtmlWebpackPlugins (entries) {
+  console.log('. Generating html webpack plugins')
+  return Object.entries(entries).map(([ name, { html } ]) => {
+    const options = {
+      title: `${name} | threejs playground`,
+      filename: path.join(name === 'main' ? '' : name, 'index.html'),
+      chunks: [ name ],
+      alwaysWriteToDisk: true
+    }
+    if (html) options.html = html
+    console.log(chalk.cyan('entry:'), name, chalk.cyan('options:\n'), options)
+    return new HtmlWebpackPlugin(options)
+  })
+}
+module.exports = {
+  entry: generateEntryPoints(entries),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js'
@@ -29,16 +48,7 @@ module.exports = {
     inline: true
   },
   plugins: [
-    ...Object.entries(entries).map(([ name, { html } ]) => {
-      const options = {
-        title: `${name} | threejs playground`,
-        filename: path.join(name === 'main' ? '' : name, 'index.html'),
-        chunks: [ name ],
-        alwaysWriteToDisk: true
-      }
-      if (html) options.html = html
-      return new HtmlWebpackPlugin(options)
-    }),
+    ...generateHtmlWebpackPlugins(entries),
     new HtmlWebpackHarddiskPlugin()
   ]
 }
