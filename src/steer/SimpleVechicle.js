b7ba1845046f1c3fef375c24d566017e00c99933
Jessada Trirongkit <jessada.trirongkit@gmail.com>
2017-12-30 15:08:27 +0700

    Seek, flee, etc on raycasted target

--- src/steer/SimpleVechicle.js
+++ src/steer/SimpleVechicle.js
@@ -13,7 +13,7 @@ class SimpleVehicle {
     velocity: THREE.Vector3,
     maxForce: number,
     maxSpeed: number,
-    rotation: THREE.Quaternion
+    rotation?: THREE.Quaternion
   ) {
     this.mass = mass
     this.position = position
@@ -26,17 +26,21 @@ class SimpleVehicle {
       up: BASIS.UP.clone()
     }
 
-    this.orientation.forward.applyQuaternion(rotation)
-    this.orientation.side.applyQuaternion(rotation)
-    this.orientation.up.applyQuaternion(rotation)
+    if (rotation) {
+      this.orientation.forward.applyQuaternion(rotation)
+      this.orientation.side.applyQuaternion(rotation)
+      this.orientation.up.applyQuaternion(rotation)
+    }
 
     this.createMesh()
   }
 
   createMesh () {
-    const geometry = new THREE.ConeGeometry(1, 5, 20)
+    const geometry = new THREE.ConeGeometry(0.25, 1, 32)
     geometry.rotateX(Math.PI / 2)
-    const material = new THREE.MeshBasicMaterial({color: 'skyblue'})
+    geometry.rotateZ(Math.PI / 3)
+
+    const material = new THREE.MeshLambertMaterial({ color: 'cyan' })
     this.mesh = new THREE.Mesh(geometry, material)
 
     this.updateMesh()
@@ -48,14 +52,48 @@ class SimpleVehicle {
     this.steer(steering)
   }
 
+  flee (target: THREE.Vector3) {
+    const desiredVelocity = target.clone().sub(this.position).normalize(this.maxSpeed)
+    const steering = desiredVelocity.negate().sub(this.velocity)
+    this.steer(steering)
+  }
+
+  arrival (target: THREE.Vector3, slowingDistance: number, stopRadius: number = 0, stopSpeed: number = 0) {
+    const targetOffset = target.clone().sub(this.position)
+    const distance = targetOffset.length()
+    const rampedSpeed = this.maxSpeed * (distance / slowingDistance)
+    const clippedSpeed = Math.min(rampedSpeed, this.maxSpeed)
+    const desiredVelocity = targetOffset.multiplyScalar(clippedSpeed / distance)
+    const steering = desiredVelocity.sub(this.velocity)
+    if (distance <= stopRadius && desiredVelocity.length() <= stopSpeed) {
+      this.velocity.set(0, 0, 0)
+      return
+    }
+    this.steer(steering)
+  }
+
   steer (steeringDirection: THREE.Vector3) {
+    const angleDiff = steeringDirection.angleTo(this.orientation.forward)
+    if (angleDiff > Math.PI / 2 && this.velocity.length() < 0.5) {
+      steeringDirection.applyAxisAngle(this.orientation.up, -angleDiff * 0.2)
+    }
     const steeringForce = steeringDirection.clone().clampLength(0, this.maxForce)
     const acceleration = steeringForce.divideScalar(this.mass)
     this.velocity.add(acceleration.clampLength(0, this.maxSpeed))
   }
 
+  separate (neighbors, radius: number) {
+    const repulsiveForce = neighbors.reduce((force: THREE.Vector3, neighbor: SimpleVehicle) => (
+      force.add(this.position.clone().sub(neighbor.position))
+    ), new THREE.Vector3(0, 0, 0))
+    repulsiveForce.normalize().divideScalar(radius)
+    const acceleration = repulsiveForce.divideScalar(this.mass)
+    this.velocity.add(acceleration)
+  }
+
   updateMesh () {
     this.mesh.position.set(this.position.x, this.position.y, this.position.z)
+    this.mesh.up = this.orientation.up
     this.mesh.lookAt(this.position.clone().add(this.orientation.forward))
   }
 
@@ -63,8 +101,12 @@ class SimpleVehicle {
     this.position.add(this.velocity)
 
     this.mesh.position.set(this.position.x, this.position.y, this.position.z)
-    const newForward = this.velocity.clone().normalize()
-    this.orientation.forward.set(newForward.x, newForward.y, newForward.z)
+    if (this.velocity.lengthSq()) {
+      const newForward = this.velocity.clone().normalize()
+      const newSide = newForward.clone().cross(this.orientation.up)
+      this.orientation.forward.set(newForward.x, newForward.y, newForward.z)
+      this.orientation.side.set(newSide.x, newSide.y, newSide.z)
+    }
 
     this.updateMesh()
   }
--- src/steer/main.js
+++ src/steer/main.js
@@ -4,32 +4,86 @@ import SimpleVechicle from './SimpleVechicle'
 
 const OrbitControls = require('three-orbit-controls')(THREE)
 
-const quaternion = new THREE.Quaternion()
-quaternion.setFromAxisAngle(new THREE.Vector3(0, 0, -1), Math.PI / 2)
-const vehicle = new SimpleVechicle(5, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0), 0.1, 0.3, quaternion)
-
 export const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 
 const scene = new THREE.Scene()
-scene.add(vehicle.mesh)
+const invisibleMaterial = new THREE.MeshBasicMaterial()
+invisibleMaterial.visible = false
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(window.innerWidth, window.innerHeight),
+  invisibleMaterial
+)
+scene.add(plane)
+
+const vehicles = []
+for (let i = 0; i < 1000; i++) {
+  const vehicle = new SimpleVechicle(
+    Math.random() * 5 + 1,
+    new THREE.Vector3(Math.random() * 100 - 50, Math.random() * 100 - 50, 0),
+    new THREE.Vector3(0, 0, 0),
+    Math.random() * 10,
+    Math.random() * 5
+  )
+  vehicles.push(vehicle)
+  scene.add(vehicle.mesh)
+}
+
+const ambientLight = new THREE.AmbientLight(0x0c0c0c)
+ambientLight.intensity = 5
+scene.add(ambientLight)
+
+const spotLight = new THREE.SpotLight(0xffffff)
+spotLight.position.set(-30, 60, 60)
+spotLight.intensity = 1
+spotLight.castShadow = true
+scene.add(spotLight)
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 75
 
 const controls = new OrbitControls(camera)
-const target = new THREE.Vector3(0, 0, 0)
-let targetAngle = 0
-let targetDir = 5
+
+const raycaster = new THREE.Raycaster()
+let mouse = new THREE.Vector2()
+
+function onMouseMove (event) {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+}
+
 function animate () {
-  target.x += targetDir
-  if (target.x > 100 || target.x < -100) targetDir *= -1
-  target.setY(50 * Math.sin(targetAngle))
-  targetAngle += Math.PI / 180
-  vehicle.seek(target)
-  vehicle.update()
+  raycaster.setFromCamera(mouse, camera)
+  const intersects = raycaster.intersectObject(plane)
+  if (intersects.length) {
+    const target = intersects[0].point
+    vehicles.forEach((v: SimpleVechicle, i: number) => {
+      if (v.position.distanceTo(target) > 30) {
+        v.arrival(target, 25, 4, 0.1)
+        // v.seek(target)
+      } else {
+        v.flee(target)
+      }
+    })
+  }
+
+  vehicles.forEach((vi: SimpleVechicle) => {
+    let neighbors = [ ]
+    for (let j = 0; j < vehicles.length; j++) {
+      const vj = vehicles[j]
+      if (vi.position.distanceTo(vj.position) <= 2) {
+        neighbors.push(vj)
+      }
+    }
+    vi.separate(neighbors, 2)
+  })
+
+  vehicles.forEach((v: SimpleVechicle) => v.update())
   controls.update()
+
   window.requestAnimationFrame(animate)
   renderer.render(scene, camera)
 }
 animate()
+
+window.addEventListener('mousemove', onMouseMove, false)
